904c504421cc7a802afcb046620809de90f5b6b5
Ivan MPR <ivsanmas@gmail.com>
2021-10-08 19:24:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,119 @@
+'use-strict';
+
+const digitsContainer = document.querySelector('.digits-container');
+const display = document.querySelector('.disp-content');
+
+const clrBtn = document.querySelector('.clear');
+const operands = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.eq');
+const muteBtn = document.querySelector('.mute-btn');
+const unmuteBtn = document.querySelector('.unmute-btn');
+
+const beep = document.querySelector('.beep');
+const equalButtonSound = document.querySelector('.ebs');
+const clearButtonSound = document.querySelector('.cbs');
+
+const allTones = document.querySelectorAll('audio');
+// Data container
+const data = {
+  string: '',
+  regex1: /[+\-*/]{1,}/,
+  regex2: /[+\-*/]/,
+  regex3: /[\.]{2,}/,
+  result: '',
+  isMuted: false,
+};
+// Clear fields and display
+function clearFields() {
+  data.string = '';
+  display.textContent = '';
+}
+// Prevent multi operator inputs
+function stopDoubleOp(str) {
+  return data.regex1.test(str);
+}
+// Prevent multi dots inputs
+function stopDoubleDots(str) {
+  // console.log(data.regex3.test(str));
+  return data.regex3.test(str);
+}
+
+// Calculate result
+function calc(string) {
+  // Split string on operator and transform elements to numbers
+  const arr = string.split(data.regex2).map(el => Number(el));
+  const [firOp, secOp] = arr;
+  const [operator] = string.match(data.regex2);
+
+  const result =
+    operator === '+'
+      ? firOp + secOp
+      : operator === '-'
+      ? firOp - secOp
+      : operator === '*'
+      ? firOp * secOp
+      : firOp / secOp;
+
+  clearFields();
+  data.result = result;
+}
+
+digitsContainer.addEventListener('click', function (e) {
+  const t = e.target;
+  // Return if click wasn't on number field
+  if (!t.classList.contains('digits')) return;
+  // Guard clause to prevent order of input(return if operator is before operand)
+  if (!data.string && t.classList.contains('operator')) return;
+  // Guard clause to prevent multiple operator inputs
+  if (stopDoubleOp(data.string) && t.classList.contains('operator')) return;
+  // Alert, clear fields and return if multiple dot inputs are attempted
+  if (stopDoubleDots(data.string)) {
+    alert('Consecutive dots input not allowed!');
+    clearFields();
+    return;
+  }
+  // Condition for playing same tone on all digits fields except '=' digit
+  if (!t.classList.contains('eq')) beep.play();
+  // If all conditions are met, concat new input to the string, update string, play tone and update display
+  data.string += t.id;
+  display.textContent = data.string;
+});
+
+equalBtn.addEventListener('click', function () {
+  equalButtonSound.play();
+  // Prevent multiple dot inputs on second possible place
+  if (stopDoubleDots(data.string)) {
+    alert('Consecutive dots input not allowed!');
+    clearFields();
+    return;
+  }
+  // Return if no operator present in the string
+  if (!data.regex1.test(data.string)) {
+    clearFields();
+    return;
+  }
+  calc(data.string);
+  // Because of timeout, calc is executed first, and than it is possible to read and display that data in the UI
+  setTimeout(() => {
+    display.textContent = data.result;
+  }, 0);
+});
+
+clrBtn.addEventListener('click', () => {
+  clearFields();
+  clearButtonSound.play();
+});
+// Sounds mute
+muteBtn.addEventListener('click', function () {
+  data.isMuted = true;
+  allTones.forEach(audio => (audio.muted = true));
+  this.classList.toggle('hidden');
+  unmuteBtn.classList.toggle('hidden');
+});
+// Sounds unmute
+unmuteBtn.addEventListener('click', function () {
+  data.isMuted = false;
+  allTones.forEach(audio => (audio.muted = false));
+  this.classList.toggle('hidden');
+  muteBtn.classList.toggle('hidden');
+});
